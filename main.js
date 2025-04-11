24c8d67955c5fccfd89e82a65dd5b9e86636b27f
Sylvia-12 <zirui.zhu@student.mahidol.ac.th>
2025-04-11 16:02:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-public class zSortyArray {
-public static void main(String[] args) {
-int[] numArray = {5, 3, 8, 1, 2};
-sorty(numArray);
-for(int i = 0; i <= numArray.length; i++) {
-System.out.print(numArray[i] + " ");
-}
-}
-public static void sorty(int[] arr) {
-for(int i = 0; i < arr.length; i++) {
-for(int j = 0; j < arr.length; j++) {
-if(arr[i] < arr[j]) {
-int temp = arr[i];
-arr[i] = arr[j];
-arr[j] = temp;
-}
-}
-}
+public class SortArray {
+    public static void main(String[] args) {
+        int[] numArray = {5, 3, 8, 1, 2};
+        sortArray(numArray);
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.print(numArray[i] + " ");
+        }
+    }
 
-}
+    public static void sortArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
